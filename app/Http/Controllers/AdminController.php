1bf83f823701c06bbdfd0f91b589d63323c1515b
Victor Danilo <victordanilo01@gmail.com>
2019-09-13 16:02:57 -0300

    Add middleware de permissão usuário

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,6 +20,7 @@ class AdminController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('permission.admin');
     }
 
     /**
